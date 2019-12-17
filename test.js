60b63160c6e7e2f488363c719ba1f1d8d919f389
jpatrickcm <jeanpatrick.94@gmail.com>
2019-12-16 23:19:08 -0500

    Caso terminado, creacion de nuevo contrato en base a pendiente a amortizar de contrato anterior.

--- test.js
+++ test.js
@@ -31,19 +31,13 @@ prestamo.fechaFin = "02-Nov-2019";
 prestamo.tiempoTranscurrido = 52;
 prestamo.estado = "ABONADO";
 
-
 /*
      Calculo del interes Final
 */
 
-// let montoInteres = prestamo.monto * prestamo.porcentajeInteres;
-// let montoInteresDiario = montoInteres / 30;
-// let montoInteresTranscurrido = montoInteresDiario * prestamo.tiempoTranscurrido;
-// prestamo.montoFinalInteres = montoInteresTranscurrido;
-
 prestamo.montoFinalInteres = CalculoMontoInteresTranscurrido(prestamo);
 
-console.log("montoFinalInteres : " + prestamo.montoFinalInteres);
+// console.log("montoFinalInteres : " + prestamo.montoFinalInteres);
 
 if (prestamo.amortizacion != "") {
     prestamo.pendienteAmortizar = ((prestamo.montoFinalInteres + prestamo.monto) - prestamo.amortizacion);
@@ -51,7 +45,23 @@ if (prestamo.amortizacion != "") {
     prestamo.pendienteAmortizar = (prestamo.montoFinalInteres + prestamo.monto);
 }
 
+console.log("\n" + "           NUEVO CONTRATO  " + "\n");
+
 console.log(prestamo);
+
+
+/**
+ * Si el pendiente a amortizar es mayor a 0, se da por finalizado el contrato/acuerdo anterior.
+ */
+
+prestamo.estado = "TERMINADO";
+console.log("                               TERMINADO ----> : " + prestamo.id +
+    "\n" + "           NUEVO CONTRATO  " + "\n");
+
+
+/**
+ * Se crea el nuevo contrato/acuerdo donde se registra el pendiente co nreferencia al acuerdo anterior
+ */
 
 prestamo = {
     id: "ApoyoMutuoI-002",
@@ -88,20 +98,68 @@ if (prestamo.amortizacion != "") {
 console.log(prestamo);
 
 
+
+console.log("\n" + "           NUEVO CONTRATO  " + "\n");
+
+prestamo = {
+    id: "ApoyoMutuoII-001",
+    prestamoOrigen: "ApoyoMutuoI-001",
+    monto: 3000.00,
+    tipoMoneda: "USD",
+    porcentajeInteres: 0.05,
+    tiempo: 8,
+    tiempoUnidad: "dia",
+    fechaInicio: "27-Nov-2019",
+    fechaFin: null,
+    tiempoTranscurrido: 0,
+    tiempoTranscurridoUnidad: "dia",
+    montoFinalInteres: 0.00,
+    prestamista: "Mariliz(Mama)",
+    beneficiario: "Jorge(Papa)",
+    amortizacion: 0.00,
+    pendienteAmortizar: 0.00,
+    flag_finalizado: false,
+    flag_diasTrancurridos: false,
+    estado: "REGISTRADO"
+};
+
+
+if (prestamo.fechaFin == undefined) {
+    prestamo.tiempoTranscurrido = CalcularTiempoTranscurrido(prestamo.fechaInicio, new Date());
+}
+prestamo.montoFinalInteres = CalculoMontoInteresTranscurrido(prestamo);
+
+if (prestamo.amortizacion != "") {
+    prestamo.pendienteAmortizar = ((prestamo.montoFinalInteres + prestamo.monto) - prestamo.amortizacion);
+} else {
+    prestamo.pendienteAmortizar = (prestamo.montoFinalInteres + prestamo.monto);
+}
+console.log(prestamo);
+
+
+
+
+
+
 function CalculoMontoInteresTranscurrido(prestamo) {
     /*
          Calculo del interes Final
     */
     let montoInteres = prestamo.monto * prestamo.porcentajeInteres;
-    let montoInteresDiario = montoInteres / 30;
+    let montoInteresDiario = montoInteres / prestamo.tiempo;
     let montoInteresTranscurrido = montoInteresDiario * prestamo.tiempoTranscurrido;
     return montoInteresTranscurrido;
 }
 
 function CalcularTiempoTranscurrido(fecha1, fecha2) {
-    let date1 = new Date(fecha1);
-    let date2 = new Date(fecha2);
+    let date1 = ObtenerFechaSinHoras(fecha1);
+    let date2 = ObtenerFechaSinHoras(fecha2);
     let Difference_In_Time = date2.getTime() - date1.getTime();
     let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
     return Difference_In_Days;
+}
+
+function ObtenerFechaSinHoras(fecha) {
+    fecha = new Date(fecha);
+    return new Date(fecha.getFullYear(), fecha.getMonth(), fecha.getDate());
 }
